1c1ba3b818124ae46383eb1e9aef14e7669d89d0
Jackson Hales <me@jacksonhales.com.au>
2019-10-12 11:12:36 +1100

    Added test project

---
+++ ShapeShowdown.Tests/TriangleTests.cs
@@ -0,0 +1,20 @@
+using MyGame.src;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class TriangleTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Triangle testTriangle = new Triangle();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
